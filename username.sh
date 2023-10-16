1085231f32bf3e9f200f7e6d1cb10bfce5488566
Carolina Caraballo <143666734+carolcv04@users.noreply.github.com>
2023-10-15 23:09:11 -0700

    Adds username.sh

--- username.sh
+++ username.sh
@@ -1,2 +1,19 @@
-#hi
+#!/bin/bash
+# username.sh
+# Carolina Caraballo Velez
+# CPSC 298
+echo "CREATE A USERNAME!"
+echo "1. Only lowercase case letters, digits & underscore"
+echo "2. MUST start with a lowercase"
+echo "3. MUST contain at least 3 but no more than 12 characters"
+echo "Enter your username: "
+read -r username
+
+while echo "$username" | grep -E -v "^[a-z0-9_]{3,12}$" > /dev/null 2>&1
+do
+	echo "HEY! Follow the rules!"
+	echo "Re-enter your username: "
+	read -r username
+done
+echo "Thank you! Username created succesfully"
 
